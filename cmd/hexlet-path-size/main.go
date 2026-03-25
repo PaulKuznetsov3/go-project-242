e9a7da2bbd87a38d01be1691d7d0677149dc0407
Pavel <pavel_iv37@mail.ru>
2026-03-25 11:50:06 +0300

    installed cli

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "log"
+    "os"
+    "context"
+
+    "github.com/urfave/cli/v3"
+)
 
 func main() {
-	fmt.Println("Hello from Hexlet!")
+    cmd := &cli.Command{
+        Name:  "hexlet-path-size",
+        Usage: "print size of a file or directory",
+        Action: func(context.Context, *cli.Command) error {
+            fmt.Println("boom! I say!")
+            return nil
+        },
+    }
+
+    if err := cmd.Run(context.Background(), os.Args); err != nil {
+        log.Fatal(err)
+    }
 }
